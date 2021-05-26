59a3fe8e7a56e9d134ffbf614c914dbcbc69ac0c
Ferdinand <ndimbecha@gmail.com>
2021-05-26 13:49:08 +0200

    review programming basics: Basics

---
+++ reviewBasics.js
@@ -0,0 +1,40 @@
+// # Review 1 - basics
+
+// NOTE! Pay close attention to creating code with good and consistent code style.
+
+// 1. Make a variable and assign an even number to that variable. Make sure that variable cannot be changed.
+
+const evenNumber = 16; //This variable cannot be changed since it is declared with const
+
+// 2. Make a variable and assign a negative odd number to that variable. Make sure that variable cannot be changed.
+const negativeNumber = -3;
+
+// 3. Make a variable and assign a string to that variable using double quotes. Make sure that variable cannot be changed.
+
+const stringDoubleQuote = "Hello World"
+// 4. Make a variable and assign a string to that variable using single quotes. Make sure that variable cannot be changed.
+
+stringSingleQuote = 'Hello World'
+
+// 5. Make a variable and assign a string to that variable using a template literal. Make sure that variable cannot be changed.
+
+const templateLiteral = `The people in the delivery room were shocked when the newborn shouted ${stringDoubleQuote}!`
+// 6. Make a variable and assign an empty string to that variable using a template literal. Make sure that variable *can* be changed.
+let emptyString = `" "` // 'let' permits changes to be made to the variable
+
+// 7. Make a variable and assign a boolean to that variable using a template literal. Make sure that variable *can* be changed.
+let booleanVariable // wrongly worded question, I think
+
+// 8. Make a variable and assign a boolean to that variable using a template literal. Make sure that variable can not be changed.
+
+// ## This question seems wrongly worded too ##
+
+
+// 9. Print the `type` of a variable that has a number value.
+console.log( typeof evenNumber)
+// 10. Print the `type` of a variable that has a string value.
+console.log( typeof stringDoubleQuote)
+
+// 11. Print the `type` of a variable that has a boolean value.
+let thisVariable = true
+console.log( typeof thisVariable)
